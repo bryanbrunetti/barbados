1e5687e2ed32312e26b5e8681bb5e887b9db47c2
Bryan Brunetti <bryan.brunetti@gmail.com>
2020-03-08 12:04:03 -0400

    fixed the SQLALCHEMY_TRACK_MODIFICATIONS config

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 
 def load(app):
     APP_ROOT = os.path.join(os.path.dirname(__file__))
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     dotenv_path = os.path.join(APP_ROOT, '.env')
     load_dotenv(dotenv_path)
     app.secret_key = os.environ["SESSION_SECRET"]
